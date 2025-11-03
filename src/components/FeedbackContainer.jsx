b7e2c82bbbbf2b2b614e3c267c48a2c9af7bbbc1
Peter Heri <peterheri@Peters-MacBook-Pro.local>
2025-11-03 20:19:45 +0300

    feat:Add product page
    Users can now access details  of specifc products, call  the vendor and request invoices so as to pey

---
+++ src/components/FeedbackContainer.jsx
@@ -0,0 +1,29 @@
+import "../styles/feedback.css";
+
+export default function FeedbackContainer({
+  name = "Jenny Doe",
+  comment = "Fast delivery, items were well received!",
+  date = "10/09/25",
+}) {
+  return (
+    <div className="feedback-container">
+      <div className="section-header">
+        <div className="user-profile">
+          <div className="user-image-container">
+            <img
+              src="https://images.unsplash.com/photo-1597393922738-085ea04b5a07?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1022"
+              alt=""
+              className="vendor-img"
+            />
+          </div>
+          <h5 className="vendor-name">{name}</h5>
+        </div>
+
+        <p className="user-comment-date">{date}</p>
+      </div>
+
+      <div className="user-rating">3stars</div>
+      <p>{comment}</p>
+    </div>
+  );
+}
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -5,8 +5,37 @@ import ProductCarousel from "../components/ProductCarousel";
 import locationIcon from "../assets/icons/location.svg";
 import ChatIcon from "../assets/icons/chat.svg?react";
 import PhoneIcon from "../assets/icons/phone.svg?react";
+import storeIcon from "../assets/icons/store.svg";
+import ClockIcon from "../assets/icons/clock.svg?react";
+import InvoiceIcon from "../assets/icons/invoice.svg?react";
+import FeedbackContainer from "../components/FeedbackContainer";
+
+import { useState } from "react";
 
 export default function ProductPage() {
+  const [text, setText] =
+    useState(`It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English.`);
+
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isStoreExpanded, setIsStoreExpanded] = useState(false);
+
+  const handleStoreExpansion = () => {
+    setIsStoreExpanded((cond) => !cond);
+  };
+
+  const MAX_WORDS = 30;
+
+  const words = text.trim().split(/\s+/);
+  const isLongText = words.length > MAX_WORDS;
+
+  const displayText = isExpanded
+    ? text
+    : words.slice(0, MAX_WORDS).join(" ") + (isLongText ? "..." : "");
+
   return (
     <main aria-label="Product Page" className="product-page">
       <header aria-label="product pager header" className="product-page-header">
@@ -19,12 +48,12 @@ export default function ProductPage() {
         </button>
       </header>
 
-      <section
+      {/* <section
         aria-label="product images preview"
         className="product-images-preview"
-      >
-        <ProductCarousel />
-      </section>
+      ></section> */}
+
+      <ProductCarousel />
 
       <section aria-label="Product Overview" className="product-overview">
         <div className="product-meta">
@@ -64,6 +93,108 @@ export default function ProductPage() {
           </button>
         </div>
       </section>
+
+      <section aria-label="product Information" className="product-info">
+        <h2>Product Details</h2>
+        <p>
+          {displayText}{" "}
+          {isLongText && (
+            <span onClick={() => setIsExpanded(!isExpanded)}>
+              {isExpanded ? "Show less" : "Read more"}
+            </span>
+          )}
+        </p>
+      </section>
+
+      <section aria-label="store Information" className="store-info">
+        <div className={`store-container ${isStoreExpanded ? "expanded" : ""}`}>
+          <div className="store-header">
+            <div className="store-header-title">
+              <img src={storeIcon} alt="" className="fa" />
+              <h4>Store Address</h4>
+            </div>
+
+            {!isStoreExpanded && (
+              <span onClick={handleStoreExpansion}>Show ⌄</span>
+            )}
+          </div>
+
+          {isStoreExpanded && (
+            <>
+              <div className="product-meta">
+                <p className="prod-location">
+                  <img src={locationIcon} alt="map" className="fa" />
+                  Nairobi{" "}
+                </p>
+                <p>Nairobi Central</p>
+              </div>
+
+              <p className="prod-specific-location">
+                Bebabeba trade centre shop no D18 grounnd floor along tom mboya
+                street
+              </p>
+
+              <div className="store-timelines">
+                <ClockIcon className="fa" />
+                <span className="store-status">Open now</span>
+                <p className="store-days">Mon - Sat</p>
+                <p className="store-hours">08:00 - 18:00</p>
+              </div>
+
+              <span className="hide-span" onClick={handleStoreExpansion}>
+                Hide ^
+              </span>
+            </>
+          )}
+        </div>
+      </section>
+
+      <section
+        aria-label="vendor history Information"
+        className="vendor-history-info"
+      >
+        <div className="vendor-history-container">
+          <div className="section-header">
+            <div className="vender-profile">
+              <div className="vendor-image-container">
+                <img
+                  src="https://images.unsplash.com/photo-1597393922738-085ea04b5a07?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1022"
+                  alt=""
+                  className="vendor-img"
+                />
+              </div>
+              <h5 className="vendor-name">Jenny Doe</h5>
+            </div>
+
+            <p>View all ads(42)</p>
+          </div>
+
+          <div className="feedback-section">
+            <div className="section-header">
+              <h4>Feedback about vendor</h4>
+              <p>view all(10)</p>
+            </div>
+            <div className="feedback-showcase">
+              <FeedbackContainer />
+              <FeedbackContainer />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer>
+        <div className="price-container">
+          <p>Total Price</p>
+          <p>
+            KSh<span>15,000</span>
+          </p>
+        </div>
+
+        <button>
+          <InvoiceIcon className="fa" />
+          Request Invoice
+        </button>
+      </footer>
     </main>
   );
 }
